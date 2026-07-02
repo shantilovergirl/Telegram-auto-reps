680e74f5ba36fc55c876fa92403286bcc1590356
Shanti <alessandradomanais07@gmail.com>
2026-07-03 03:05:34 +0800

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,17 @@
+if "hi" in text:
+    await update.message.reply_text("Hello po! Donny Pangilinan is offline for a while, will reply agad when am online. type "pafix" if magpapaayos po para lumabas yung rate ko")
+
+elif "up" in text:
+    await update.message.reply_text("up, ano po need?")
+
+elif "pafix" in text:
+    await update.message.reply_text("Hello, my rate for fixing couln't send message account is 35 if selfie po need, 50 po if ID. drop email and password lang po, may ginagawa lang saglit")
+
+elif "gcash" in text:
+    await update.message.reply_text("via qr po, wait")
+
+elif "maya" in text:
+    await update.message.reply_text("have po, wait lang)
+
+else:
+    await update.message.reply_text("pa-wait na lang po si Donny")
